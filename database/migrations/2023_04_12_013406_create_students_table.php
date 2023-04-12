65e681c163f11751959bfe0919319d75aa6370c4
rismaintan <rismaintan60@gmail.com>
2023-04-12 08:38:51 +0700

    create model student

--- database/migrations/2023_04_12_013406_create_students_table.php
+++ database/migrations/2023_04_12_013406_create_students_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string(('name'));
+            $table->string(('course'));
+            $table->string(('email'));
+            $table->string(('phone'));
             $table->timestamps();
         });
     }

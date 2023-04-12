96cb68e6d7373368370fd1a57c968d3be9dad36f
rismaintan <rismaintan60@gmail.com>
2023-04-12 12:04:52 +0700

    update student controller

---
+++ app/Http/Controllers/Api/StudentController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class StudentController extends Controller
+{
+    public function index(){
+        $students = Student::all();
+        if($students->count()>0){
+            return response()->json([
+                'status' => 200,
+                'students' => $students
+            ], 200);
+        }else{
+            return response()->json([
+                'status' => 404,
+                'message' => 'no record'
+            ], 404);
+        }
+    }
+
+    public function store(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string:191',
+            'course' => 'required|string:191',
+            'email' => 'required|email:191',
+            'phone' => 'required|digits:10',
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'status' => 422,
+                'errors'    => $validator->messages()
+            ], 422);
+
+        }else{
+            $students = Student::create([
+                'name'  => $request->name,
+                'course'  => $request->course,
+                'email'  => $request->email,
+                'phone'  => $request->phone,
+            ]);
+
+            if($students){
+                return response()->json([
+                    'status' =>200,
+                    'message' => "Student Created Successfully"
+                ], 200);
+            }else{
+                return response()->json([
+                    'status'    => 200,
+                    'message'   => "Something Went Wrong"
+                ], 500); 
+
+            }
+        }
+    }
+}

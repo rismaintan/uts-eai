3c1780e8197774640513e59ce0f4882839544d8c
rismaintan <rismaintan60@gmail.com>
2023-05-03 16:14:11 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -14,14 +14,14 @@ Route::get('students', [StudentController::class, 'index']);
 Route::post('students', [StudentController::class, 'store']);  
 Route::get('students/{id}', [StudentController::class, 'show']); 
 
-Route::get('students/{id}/edit', [StudentController::class, 'edit']);  
+// Route::get('students/{id}/edit', [StudentController::class, 'edit']);  
 Route::put('students/{id}/edit', [StudentController::class, 'update']); 
 
 Route::delete('students/{id}/delete', [StudentController::class, 'destroy']); 
 
 Route::get('wali', [WaliController::class, 'index']); 
 Route::post('wali', [WaliController::class, 'store']); 
-Route::get('wali/{id}', [WaliController::class, 'show']);
-Route::put('wali/{id}/edit', [WaliController::class, 'update']);  
-Route::delete('wali/{id}/delete', [WaliController::class, 'destroy']); 
+// Route::get('wali/{id}', [WaliController::class, 'show']);
+// Route::put('wali/{id}/edit', [WaliController::class, 'update']);  
+// Route::delete('wali/{id}/delete', [WaliController::class, 'destroy']); 
  

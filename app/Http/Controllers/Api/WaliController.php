47cf597fd2468c3688e29ecbf587c07d8c41f85b
rismaintan <rismaintan60@gmail.com>
2023-05-03 16:17:33 +0700

    update v2

--- app/Http/Controllers/Api/WaliController.php
+++ app/Http/Controllers/Api/WaliController.php
@@ -58,72 +58,72 @@ class WaliController extends Controller
         }
     }
 
-    public function show($id){
-        $wali = Wali::find($id);
-        if($wali){
-            return response()->json([
-                'status'    => 200,
-                'wali'      => $wali
-            ], 200); 
+    // public function show($id){
+    //     $wali = Wali::find($id);
+    //     if($wali){
+    //         return response()->json([
+    //             'status'    => 200,
+    //             'wali'      => $wali
+    //         ], 200); 
             
-        }else{
-            return response()->json([
-                'status'    => 404,
-                'message'   => "No Such Wali Found!"
-            ], 404); 
-        }
-    }
+    //     }else{
+    //         return response()->json([
+    //             'status'    => 404,
+    //             'message'   => "No Such Wali Found!"
+    //         ], 404); 
+    //     }
+    // }
 
-    public function update(Request $request, int $id){
-        $validator = Validator::make($request->all(), [
-            'nama'  => 'required|string:191',
-            'pekerjaan' => 'required|string:191',
-            'student_id' => 'required'
-        ]);
-        if($validator->fails()){
-            return response()->json([
-                'status' => 422,
-                'errors'    => $validator->messages()
-            ], 422);
+    // public function update(Request $request, int $id){
+    //     $validator = Validator::make($request->all(), [
+    //         'nama'  => 'required|string:191',
+    //         'pekerjaan' => 'required|string:191',
+    //         'student_id' => 'required'
+    //     ]);
+    //     if($validator->fails()){
+    //         return response()->json([
+    //             'status' => 422,
+    //             'errors'    => $validator->messages()
+    //         ], 422);
 
-        }else{
-            $wali = Wali::find($id);
+    //     }else{
+    //         $wali = Wali::find($id);
            
-            if($wali){
-                $wali->update([
-                    'nama'  => $request->nama,
-                    'pekerjaan' => $request->pekerjaan,
-                    'student_id'  => $request->student_id,
-                ]);
+    //         if($wali){
+    //             $wali->update([
+    //                 'nama'  => $request->nama,
+    //                 'pekerjaan' => $request->pekerjaan,
+    //                 'student_id'  => $request->student_id,
+    //             ]);
 
-                return response()->json([
-                    'status' =>200,
-                    'message' => "Wali Updated Successfully"
-                ], 200);
-            }else{
-                return response()->json([
-                    'status'    => 404,
-                    'message'   => "No Such Wali Found!"
-                ], 404); 
+    //             return response()->json([
+    //                 'status' =>200,
+    //                 'message' => "Wali Updated Successfully"
+    //             ], 200);
+    //         }else{
+    //             return response()->json([
+    //                 'status'    => 404,
+    //                 'message'   => "No Such Wali Found!"
+    //             ], 404); 
 
-            }
-        }
-    }
+    //         }
+    //     }
+    // }
 
-    public function destroy($id){
-        $wali= Wali::find($id);
-        if($wali){
-            $wali->delete();
-            return response()->json([
-                'status'    => 200,
-                'message'   => "Wali Deleted Successfuly!"
-            ], 200); 
-        }else{
-            return response()->json([
-                'status'    => 404,
-                'message'   => "No Such Wali Found!"
-            ], 404); 
-        }
-    }
+    // public function destroy($id){
+    //     $wali= Wali::find($id);
+    //     if($wali){
+    //         $wali->delete();
+    //         return response()->json([
+    //             'status'    => 200,
+    //             'message'   => "Wali Deleted Successfuly!"
+    //         ], 200); 
+    //     }else{
+    //         return response()->json([
+    //             'status'    => 404,
+    //             'message'   => "No Such Wali Found!"
+    //         ], 404); 
+    //     }
+    // }
 
 }
